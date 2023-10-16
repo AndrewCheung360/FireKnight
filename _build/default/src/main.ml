7d3769833099d17eb6405b0f1bf6f99b3807362c
Andrew Cheung <aec295@cornell.edu>
2023-10-16 11:48:13 -0400

    Loaded spritesheet
    - Added texture loading functionality

--- _build/default/src/main.ml
+++ _build/default/src/main.ml
@@ -1,25 +1,27 @@
 open Constants
 open Raylib
+open Color
 let width = Constants.screen_width
 let height = Constants.screen_height
 let fps = Constants.fps
-let atlas = load_texture("../assets/characters/Fire_Knight/Fire_Knight_SpriteSheet.png")
+
 
 
 let setup () =
-  Raylib.init_window width height "Fire Knight";
-  Raylib.set_target_fps fps
+  init_window width height "Fire Knight";
+  set_target_fps fps;
+  let tex_knight = load_texture("assets/characters/Fire_Knight/Fire_Knight_SpriteSheet.png") in
 
+  tex_knight
   
 
-let rec loop () =
-  if Raylib.window_should_close () then Raylib.close_window ()
+let rec loop tex_knight =
+  if window_should_close () then close_window ()
   else
-    let open Raylib in
     begin_drawing ();
-    clear_background Color.raywhite;
-    draw_texture atlas 0 0 Color.raywhite;
+    clear_background raywhite;
+    draw_texture tex_knight 0 0 raywhite;
     end_drawing ();
-    loop ()
+    loop tex_knight
 
 let () = setup () |> loop
