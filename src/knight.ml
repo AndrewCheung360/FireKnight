422d0fc36849a69a8be771c8fae9f4691f567d8d
Andrew Cheung <aec295@cornell.edu>
2023-10-18 22:33:02 -0400

    Implemented State Management
    - Created StateManagement module

--- src/knight.ml
+++ src/knight.ml
@@ -2,11 +2,13 @@ module Knight = struct
   open Constants
   open Raylib
   open Frames.KnightFrames
+  open States.StateManager
 
   type t = {
     mutable position : Vector2.t;
     mutable velocity : Vector2.t;
     animations : Sprites.AnimatedSprite.t;
+    mutable state : States.StateManager.t;
   }
 
   let create_knight_animation () =
@@ -23,23 +25,30 @@ module Knight = struct
     in
     let position = Vector2.create 0. Constants.ground_y in
     let velocity = Vector2.create 0. 0. in
-    { position; velocity; animations }
+    let state = Idle in
+    { position; velocity; animations; state }
+
+  let handle_idle knight =
+    Sprites.AnimatedSprite.switch_animation knight.animations "idle"
+
+  let handle_run knight =
+    Sprites.AnimatedSprite.switch_animation knight.animations "run";
+    let knight_vel = Vector2.x knight.velocity in
+    let vel_x =
+      if knight.state = RunRight then
+        max (-.Constants.max_vel_x) (knight_vel -. Constants.accel_x)
+      else min Constants.max_vel_x (knight_vel +. Constants.accel_x)
+    in
+    let new_position = Vector2.add knight.position (Vector2.create vel_x 0.0) in
+    let new_x = min Constants.left_boundary (Vector2.x new_position) in
+    let new_x = max Constants.right_boundary new_x in
+    knight.velocity <- Vector2.create vel_x (Vector2.y knight.velocity);
+    knight.position <- Vector2.create new_x (Vector2.y knight.position)
 
   let handle_input knight =
-    if is_key_down Key.D || is_key_down Key.A then begin
-      Sprites.AnimatedSprite.switch_animation knight.animations "run";
-      let vel_x =
-        if is_key_down Key.D then -.Constants.move_vel else Constants.move_vel
-      in
-      let new_position =
-        Vector2.add knight.position (Vector2.create vel_x 0.0)
-      in
-      let new_x = min Constants.left_boundary (Vector2.x new_position) in
-      let new_x = max Constants.right_boundary new_x in
-      knight.velocity <- Vector2.create vel_x (Vector2.y knight.velocity);
-      knight.position <- Vector2.create new_x (Vector2.y knight.position)
-    end
-    else Sprites.AnimatedSprite.switch_animation knight.animations "idle"
+    knight.state <- handle_key_input ();
+    if knight.state = RunRight || knight.state = RunLeft then handle_run knight
+    else handle_idle knight
 
   let update knight =
     handle_input knight;
