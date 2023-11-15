41834ec83da7c1b5040b180c90af545074dc0488
Andrew Cheung <aec295@cornell.edu>
2023-11-15 18:19:33 -0500

    Added doc and mli for background

---
+++ lib/background.mli
@@ -0,0 +1,14 @@
+(** The Background module handles the background rendering. *)
+
+module Background :
+  sig
+    (** Reference to the ice background texture. *)
+    val ice_background_texture : Raylib.Texture.t option ref
+
+    (** [initialize ()] loads, initializes, and returns the ice background 
+        texture. *)
+    val initialize : unit -> Raylib.Texture.t
+
+    (** [draw_ice_background texture] draws the ice background on the screen. *)
+    val draw_ice_background : Raylib.Texture.t -> unit
+  end
