762e461db7bb021c371632aa90e64ac3aba09704
Brandon O'Connor <bwo9@cornell.edu>
2023-11-15 22:35:17 -0500

    fixed formatting for documentation

--- lib/constants.mli
+++ lib/constants.mli
@@ -1,31 +1,45 @@
-(** Constants related to the game *)
 module Constants : sig
-  (** Width of the game screen *)
+  (** Constants related to the game *)
+
   val screen_width : int
-  (** Height of the game screen *)
+  (** Width of the game screen *)
+
   val screen_height : int
-  (** Frames per second for the game *)
+  (** Height of the game screen *)
+
   val fps : int
-  (** Maximum x-velocity for the knight*)
+  (** Frames per second for the game *)
+
   val max_vel_x : float
-  (** Maximum jumping/falling velocity for the knight*)
+  (** Maximum x-velocity for the knight*)
+
   val max_vel_y : float
-  (** Constant to cause windup in any horizontal input direction*)
+  (** Maximum jumping/falling velocity for the knight*)
+
   val accel_x : float
-  (** Pixel of the floor*)
+  (** Constant to cause windup in any horizontal input direction*)
+
   val ground_y : float
-  (**Left most x-position of the knight*)
+  (** Pixel of the floor*)
+
   val left_boundary : float
-  (**Top most y-position of the knight*)
+  (**Left most x-position of the knight*)
+
   val upper_boundary : float
-  (**Right most x-position of the knight*)
+  (**Top most y-position of the knight*)
+
   val right_boundary : float
-  (**Scaling factor for the sprite of the knight*)
+  (**Right most x-position of the knight*)
+
   val knight_scale : float
-  (**Scaling factor for the sprite of the boss*)
+  (**Scaling factor for the sprite of the knight*)
+
   val frost_guardian_scale : float
-  (** Gravity constant applied to y-velocity of the knight*)
+  (**Scaling factor for the sprite of the boss*)
+
   val grav : float
-  (** Initial velocity in the upwards y-direction upon jumping*)
+  (** Gravity constant applied to y-velocity of the knight*)
+
   val jump_force : float
+  (** Initial velocity in the upwards y-direction upon jumping*)
 end
--- lib/frames.mli
+++ lib/frames.mli
@@ -1,49 +1,72 @@
-(** Loading in frame data from a json file and create a sprite *)
 module FrameDataLoader : sig
-  (** Raylib type for frame data. *)
+  (** Loading in frame data from a json file and create a sprite *)
+
   type frame = Sprites.AnimatedSprite.frame
-  (**load_frame_data should read the json file from filepath and based on anim
-     name extract the frame data and durations into a tuple*)
+  (** Raylib type for frame data. *)
+
   val load_frame_data :
     string -> string -> Sprites.AnimatedSprite.frame array * float array
+  (**load_frame_data should read the json file from filepath and based on anim
+     name extract the frame data and durations into a tuple*)
 end
 
-(** Frame data of the knight *)
 module KnightFrames : sig
-  (** Representation type of the frame data. *)
+  (** Frame data of the knight *)
+
   type frame = FrameDataLoader.frame
-  (** [jsonfile] Directory of the jsonfile containing frame data for the knight. *)
+  (** Representation type of the frame data. *)
+
   val jsonfile : string
-    (** [idle] Frames and durations for the idle animation of the knight. *)
+  (** [jsonfile] Directory of the jsonfile containing frame data for the knight. *)
+
   val idle : FrameDataLoader.frame array * float array
-    (** [run] Frames and durations for the jumping animation of the knight. *)
+  (** [idle] Frames and durations for the idle animation of the knight. *)
+
   val run : FrameDataLoader.frame array * float array
-    (** [jump] Frames and durations for the jumping animation of the knight. *)
+  (** [run] Frames and durations for the jumping animation of the knight. *)
+
   val jump : FrameDataLoader.frame array * float array
-    (** [fall] Frames and durations for the falling animation of the knight. *)
+  (** [jump] Frames and durations for the jumping animation of the knight. *)
+
   val fall : FrameDataLoader.frame array * float array
-    (** [atk1] Frames and durations for the first attack animation of the knight. *)
+  (** [fall] Frames and durations for the falling animation of the knight. *)
+
   val atk1 : FrameDataLoader.frame array * float array
-    (** [atk2] Frames and durations for the second attack animation of the knight. *)
+  (** [atk1] Frames and durations for the first attack animation of the knight. *)
+
   val atk2 : FrameDataLoader.frame array * float array
-    (** [atk3] Frames and durations for the third attack animation of the knight. *)
+  (** [atk2] Frames and durations for the second attack animation of the knight. *)
+
   val atk3 : FrameDataLoader.frame array * float array
-    (** [ult] Frames and durations for the ultimate attack animation of the knight. *)
+  (** [atk3] Frames and durations for the third attack animation of the knight. *)
+
   val ult : FrameDataLoader.frame array * float array
+  (** [ult] Frames and durations for the ultimate attack animation of the
+      knight. *)
 end
 
-(** Frame data of the Frost Guardian *)
 module FrostGuardianFrames : sig
-  (** Representation type of the frame data. *)
+  (** Frame data of the Frost Guardian *)
+
   type frame = FrameDataLoader.frame
-  (** [jsonfile] Directory of the jsonfile containing frame data for the frost guardian. *)
+  (** Representation type of the frame data. *)
+
   val jsonfile : string
-  (** [idle] Frames and durations for the idle animation of the frost guardian. *)
+  (** [jsonfile] Directory of the jsonfile containing frame data for the frost
+      guardian. *)
+
   val idle : FrameDataLoader.frame array * float array
-  (** [walk] Frames and durations for the walking animation of the frost guardian. *)
+  (** [idle] Frames and durations for the idle animation of the frost guardian. *)
+
   val walk : FrameDataLoader.frame array * float array
-  (** [punch] Frames and durations for the punching animation of the frost guardian. *)
+  (** [walk] Frames and durations for the walking animation of the frost
+      guardian. *)
+
   val punch : FrameDataLoader.frame array * float array
-  (** [intro] Frames and durations for the intro animation of the frost guardian. *)
+  (** [punch] Frames and durations for the punching animation of the frost
+      guardian. *)
+
   val intro : FrameDataLoader.frame array * float array
+  (** [intro] Frames and durations for the intro animation of the frost
+      guardian. *)
 end
